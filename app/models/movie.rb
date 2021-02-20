6e83f49c336877b38c52be8939f926b95a07bff8
YCWangVince <wyc2016@mail.ustc.edu.cn>
2021-02-20 17:07:39 +0000

    change

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ActiveRecord::Base
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+    
+    def self.all_ratings()
+        @all_ratings
+    end
+        
+    
+    def self.with_ratings(rating_list)
+        if rating_list.nil? 
+            return self.all
+        end
+        self.where("params[:rating] IN rating_list")
+    end
 end
